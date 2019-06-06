2381bd1ba3aa1293967910b610483cbb05d61957
Ramy Deeb <me@ramydeeb.com>
2019-06-06 11:15:02 -0500

    Initial commit

---
+++ feature_flipper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Feature Flags
+ * Plugin URI:  https://github.com/rdeeb/wp_feature_flags
+ * Description: A feature solution for Wordpress Developers.
+ * Version:     0.0.1
+ * Author:      Ramy Deeb
+ * Author URI:  https://ramydeeb.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp_ff
+ * Domain Path: /languages
+ */
